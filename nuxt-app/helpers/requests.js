d6ef9eecaf2e790d9cc2af255ec1b4e5c66782ee
BrunoPO <BrunoPO9896@gmail.com>
2020-07-05 21:24:20 -0300

    Organize request file

--- nuxt-app/helpers/requests.js
+++ nuxt-app/helpers/requests.js
@@ -1,32 +1,12 @@
 import CONSTANTS from '@/helpers/constants';
 
-export default {
-  get: {
-    async Recipe({ store, params }) {
-      if (params.id && store.state.query) {
-        const requestObj = {
-          operationName: null,
-          query: `{recipe(id: "${params.id}") {id title thumbnail previewText}}`,
-          variables: {},
-        };
-
-        return fetch(CONSTANTS.API_URL, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestObj),
-        }).then((response) => response.json())
-          .then((response) => response.data.recipe).catch(() => undefined);
-      }
-      return undefined;
-    },
-    async Recipes(query, originalFrom) {
-      const from = originalFrom || 0;
+const requests = {
+  generic: {
+    query(query) {
       const requestObj = {
         operationName: null,
-        query: `{recipes(query: "${query}", from:${from} , pageSize: 12) {id title thumbnail previewText}}`,
         variables: {},
+        query,
       };
 
       return fetch(CONSTANTS.API_URL, {
@@ -36,7 +16,34 @@ export default {
         },
         body: JSON.stringify(requestObj),
       }).then((response) => response.json())
-        .then((response) => response.data.recipes).catch(() => undefined);
+        .catch(() => undefined);
+    },
+  },
+  get: {
+    async Recipe({ store, params }) {
+      if (params.id && store.state.query) {
+        const query = `{recipe(id: "${params.id}") {id title thumbnail previewText}}`;
+        return requests.generic.query(query)
+          .then((response) => response.data.recipe);
+      }
+      return undefined;
+    },
+    async Recipes(searchQuery, originalFrom) {
+      const from = originalFrom || 0;
+      const query = `{recipes(query: "${searchQuery}", from:${from} , pageSize: 12) {id title thumbnail previewText}}`;
+      return requests.generic.query(query)
+        .then((response) => response.data.recipes);
+    },
+    async Favorites(store) {
+      let query = '';
+      Object.keys(store.state.wishlist).forEach((curr, index) => {
+        query += `Index${index}: recipe(id: "${curr}") {id title thumbnail previewText}`;
+      });
+      query = `{${query}}`;
+      return requests.generic.query(query)
+        .then((response) => Object.values(response.data));
     },
   },
 };
+
+export default requests;
